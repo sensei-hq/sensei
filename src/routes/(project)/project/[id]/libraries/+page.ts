515a694a7a3ee7d58a709deb5a0b6a0355b5e79b
Sensei HQ <hi@sensei-hq.com>
2026-05-04 11:54:28 -0500

    fix(app): fix null guard order and type in libraries load function

--- src/routes/(project)/project/[id]/libraries/+page.ts
+++ src/routes/(project)/project/[id]/libraries/+page.ts
@@ -1,11 +1,13 @@
+import type { PageLoad } from './$types.js';
 import { senseiApi } from '$lib/api.js';
 import { appState } from '$lib/appstate.svelte.js';
 
-export async function load({ params, parent }: any) {
+export const load: PageLoad = async ({ params, parent }) => {
   const { project } = await parent();
   const api = senseiApi(appState.port);
   const data = await api.getProjectLibraries(params.id);
-  const wrappedCount = data.libraries.filter((l: any) => l.has_instruments).length;
-  const unwrappedCount = data.libraries.length - wrappedCount;
-  return { project, libraries: data.libraries ?? [], wrappedCount, unwrappedCount };
-}
+  const libraries = data.libraries ?? [];
+  const wrappedCount = libraries.filter((l: any) => l.has_instruments).length;
+  const unwrappedCount = libraries.length - wrappedCount;
+  return { project, libraries, wrappedCount, unwrappedCount };
+};
